816f49ca850267e3aea92dee848bb6da01959265
Sowjanya45 <sowjanyakanukollu@gmail.com>
2025-10-18 19:07:20 +0530

    Updates

---
+++ precache-manifest.08d7d0cf1c0d42d360b4ba7791c09715.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a197851b9777370e6a662a07c516da",
+    "url": "/Hangman-SDC/index.html"
+  },
+  {
+    "revision": "65be30c6433ff7ad650b",
+    "url": "/Hangman-SDC/static/css/main.e6bc1ce3.chunk.css"
+  },
+  {
+    "revision": "85331343ad2948b1f407",
+    "url": "/Hangman-SDC/static/js/2.869be2d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Hangman-SDC/static/js/2.869be2d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65be30c6433ff7ad650b",
+    "url": "/Hangman-SDC/static/js/main.1972420a.chunk.js"
+  },
+  {
+    "revision": "04d17596dfe400647e9e",
+    "url": "/Hangman-SDC/static/js/runtime-main.7dace3d2.js"
+  }
+]);
